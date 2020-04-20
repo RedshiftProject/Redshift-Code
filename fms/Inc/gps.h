35a791009cf4a8cdcdeea66cef21e2f849b6247e
Dash <shaghoolian_daniel@hotmail.fr>
2020-04-20 21:59:28 +0200

    Played around with UART and GPS
    Non-blocking UART (DMA/Interrupts) obtaining speeds up to 3Mbaud
    Important ! Do not put HAL_Delay(), it breaks UART communication (dunno why)
    DMA doesn't work on UARTx unless UARTx has its interrupts added/turned on : https://community.st.com/s/question/0D50X00009XkfT3SAJ/bug-uarthandletypedefgstate-not-handled-correctly-when-working-in-dma-mode
    GPS/NMEA frames needs further parsing if data is used for computation on board/during flight, otherwise, record directly stuff arriving through

--- fms/Inc/gps.h
+++ fms/Inc/gps.h
@@ -7,15 +7,27 @@ typedef struct
 	uint8_t id;
 	uint16_t len;		   //Length of the payload. Does not include cls, id, or checksum bytes
 	uint16_t counter;	  //Keeps track of number of overall bytes received. Some responses are larger than 255 bytes.
-	uint16_t startingSpot; //The counter value needed to go past before we begin recording into payload array
-	uint8_t *payload;
+	//uint16_t startingSpot; //The counter value needed to go past before we begin recording into payload array
+	uint8_t payload[512];
 	uint8_t checksumA; //Given to us from module. Checked against the rolling calculated A/B checksums.
 	uint8_t checksumB;
 } ubxPacket;
 
-// Function prototypes
+typedef struct
+{
+	uint8_t talker[2];
+	uint8_t sen_form[3];
+	uint16_t len;		//Length of the payload (number of elements, not necessarily uint8_t). Does not include cls, id, or checksum bytes
+	uint8_t data_field[74];	//Only the data field (starting with ',' / ending before '*'). NMEA data field is limited to 74 characters
+	uint16_t counter;	//Length of the whole NMEA sentence
+	uint8_t checksum;
+} nmeaPacket;
 
-uint16_t ubx_checksum(uint8_t* frame, int size);
+// Function prototypes
+uint8_t UBX_parser(UART_HandleTypeDef* huart,uint8_t* rx_string, ubxPacket* msg);
+void UBX_checksum(ubxPacket* msg);
 uint8_t UBX_isChecksum(ubxPacket msg);
-uint8_t NMEA_checksum(uint8_t* frame, int size);
+uint8_t NMEA_parser(uint8_t* rx_string, nmeaPacket* msg);
+void NMEA_checksum(nmeaPacket* msg);
+uint8_t NMEA_isChecksum(nmeaPacket msg);
 #endif
